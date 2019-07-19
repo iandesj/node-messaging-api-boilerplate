3fc9d4712651b99d20c1f4551046070636d62b3f
Ian DesJardins <ian.d.desjardins@gmail.com>
2019-07-18 23:08:55 -0400

    socketio config updated to check token id for auth to socket connection using firebase

--- config/socketio.js
+++ config/socketio.js
@@ -2,15 +2,32 @@
  * socket.io middleware for authentication on connection to an event
  */
 
+const quoteFirebaseAdmin
+ = require('../lib/firebase').initializeQuoteAppFirebaseAdmin();
+
 module.exports = {
   authentication: (socket, next) => {
-    // TODO: DO AUTH STUFF
-    next();
+    const firebaseIdToken = socket.handshake.query.firebaseIdToken;
+    const messageThreadEvent = socket.handshake.query.messageThreadEvent;
+    if (firebaseIdToken && messageThreadEvent) {
+      quoteFirebaseAdmin.auth().verifyIdToken(firebaseIdToken)
+        .then(decodedToken => {
+          next();
+        })
+        .catch(error => {
+          console.log(error);
+          next(new Error('Authentication error'));
+        });
+    } else {
+      // add block to check if request came from OSB and auth OSB on connection
+      next(new Error('Authentication error'));
+    }
   },
   connection: (socket) => {
     console.log('Socket.io connection started...');
-    socket.emit('test-emit-snd', { connected: true });
-    socket.on('test-event-rcv', function (data) {
+    const messageThreadEvent = socket.handshake.query.messageThreadEvent;
+    socket.emit(`${messageThreadEvent}-connected`, { connected: true });
+    socket.on(messageThreadEvent, function (data) {
       console.log(data);
     });
   }
