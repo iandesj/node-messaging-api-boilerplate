28686b07a24465b03ddf6c852b75cf66749fdd65
Ian DesJardins <ian.d.desjardins@gmail.com>
2019-07-18 20:52:05 -0400

    change requires to create server and require io

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ require('dotenv').config();
 
 const fs = require('fs');
 const join = require('path').join;
-const express = require('express');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const mongoose = require('mongoose');
 const passport = require('passport');
 const config = require('./config');
@@ -22,7 +24,6 @@ const config = require('./config');
 const models = join(__dirname, 'app/models');
 const port = process.env.PORT || 3000;
 
-const app = express();
 const connection = connect();
 
 /**
@@ -51,7 +52,7 @@ connection
 
 function listen() {
   if (app.get('env') === 'test') return;
-  app.listen(port);
+  server.listen(port);
   console.log('Express app started on port ' + port);
 }
 
